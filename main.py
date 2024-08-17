ce187a93f0451efbfc5ffb8f7e623e92a6c264b7
2009 <2009@git.com>
2024-08-17 18:01:41 +0330

    add BPMN to yaml model

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 import json
 import yaml
+
+with open('BPMN-Network-Model\\model1.yml', 'r') as file:
+    configuration = yaml.safe_load(file)
+
+with open('config.json', 'w') as json_file:
+    json.dump(configuration, json_file)
+
+output = json.dumps(json.load(open('config.json')), indent=2)
+print(output)
