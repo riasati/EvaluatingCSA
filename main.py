7a3741dc1abe214cafe74bb7b78e82c7901ae432
2009 <2009@git.com>
2024-08-27 10:04:25 +0330

    add graph pictures for model1

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import json
 import yaml
 
-with open('BPMN-Network-Model\\model1.yml', 'r') as file:
+with open('BPMN-Network-Model/Model1/model1.yml', 'r') as file:
     configuration = yaml.safe_load(file)
 
 with open('config.json', 'w') as json_file:
