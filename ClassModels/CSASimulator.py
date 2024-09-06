5b13d170ff55585859a54575a70a2d233f6e86b2
mohammadmahdisoori <mohammadmahdisoori@gmail.com>
2024-09-06 23:13:54 +0330

    add CSA code

--- ClassModels/CSASimulator.py
+++ ClassModels/CSASimulator.py
@@ -1,19 +1,46 @@
-import Attacker
-import NetworkState
+import copy
+import random
+
+from ClassModels.Attacker import Attacker
+from ClassModels.NetworkState import NetworkState
+
+
 class CSA:
-    def __init__(self, attacker: Attacker, network_state: NetworkState):
+    def __init__(self, attacker: Attacker, network_state: NetworkState, csa_correctness: float, probabilities: list, is_random_csa: bool):
         self.attacker = attacker
         self.network_state = network_state
-        self.network_state_simulator = network_state
+        self.network_state_simulator = copy.deepcopy(network_state)
+        self.csa_correctness = csa_correctness
+        self.probabilities = probabilities
+        self.is_random_csa = is_random_csa
+        if len(self.probabilities) != 4:
+            raise "you have to provide four probabilities for creating fake change in one host."
 
-    def report_initial_state(self) -> None:
-        # Calculate Vulnerability from initial state and result is file
-        return None
+    def report_current_state(self) -> float:
+        if self.is_random_csa:
+            self.csa_correctness = random.random()
+        if random.random() > self.csa_correctness:
+            different_host = self.network_state.which_host_is_different(self.network_state_simulator)
+            if different_host is None:
+                business_factor = self.network_state_simulator.calculate_business_factor_with_state()
+                return business_factor
+            self.network_state_simulator.fake_change_in_host(different_host, self.probabilities)
+            business_factor = self.network_state_simulator.calculate_business_factor_with_state()
+            self.network_state_simulator.hosts = copy.deepcopy(self.network_state.hosts)
+            return business_factor
 
-    def report_current_state(self) -> None:
-        # estimate current state with attacker and simulator state
-        return None
+        else:
+            self.network_state_simulator.hosts = copy.deepcopy(self.network_state.hosts)
+            business_factor = self.network_state_simulator.calculate_business_factor_with_state()
+            return business_factor
 
-    def report_project_state(self) -> None:
-        # estimate project state with attacker and simulator state
-        return None
+    def report_project_state(self) -> float:
+        current_state_in_network = copy.deepcopy(self.network_state.hosts)
+        current_state_in_network_simulator = copy.deepcopy(self.network_state_simulator.hosts)
+        first_node, second_node = self.attacker.get_future_nodes_in_path()
+        if first_node != "None":
+            self.network_state.real_change_in_network(self.attacker, first_node, second_node)
+        business_factor = self.report_current_state()
+        self.network_state.hosts = current_state_in_network
+        self.network_state_simulator.hosts = current_state_in_network_simulator
+        return business_factor
--- main.py
+++ main.py
@@ -1,23 +1,31 @@
 import json
 import yaml
 from ClassModels.BPMN import BPMN
+from ClassModels.CSASimulator import CSA
 from ClassModels.NetworkState import NetworkState
 from ClassModels.Attacker import Attacker
 from pprint import pprint
 
-with open('BPMN-Network-Model/Model1/model1.yml', 'r') as file:
-    configuration = yaml.safe_load(file)
-
-with open('config.json', 'w') as json_file:
-    json.dump(configuration, json_file)
-
-output = json.dumps(json.load(open('config.json')), indent=2)
-print(output)
+# with open('BPMN-Network-Model/Model1/model1.yml', 'r') as file:
+#     configuration = yaml.safe_load(file)
+#
+# with open('config.json', 'w') as json_file:
+#     json.dump(configuration, json_file)
+#
+# output = json.dumps(json.load(open('config.json')), indent=2)
+# print(output)
 
 a = json.load(open('config.json'))
 b = BPMN(a["BPMN"])
 c = NetworkState(a["Network"])
 d = Attacker(a["Attack"])
+
+
+
+z1 = CSA(d, c, 0.5, [50, 50, 50, 50], False)
+z2 = CSA(d, c, 0.7, [50, 50, 50, 50], False)
+z3 = CSA(d, c, 0.9, [50, 50, 50, 50], False)
+z4 = CSA(d, c, 0.5, [50, 50, 50, 50], True)
 
 b.calculate_business_importance()
 b.calculate_process_priority()
@@ -35,21 +43,147 @@ c.add_host_importance(b)
 #h = d.is_attack_successful("A", g)
 #i = d.create_attack_path("A", c.hosts_configuration, [])
 
-#d.create_numbers_of_attack_path("A", c.hosts_configuration, 10000)
+d.create_numbers_of_attack_path("A", c.hosts_configuration, 10000)
 #pprint(d.attack_path_list_object)
+print(d.attack_path_list_object.keys())
+#d.fill_current_attack_path(list(d.attack_path_list_object.keys())[1])
+d.fill_current_attack_path('["A", "C", "E", "G", "I", "K", "None"]')
 
-e = c.real_change_in_network(d, "A", "B")
-print("Attack From A to B")
+e = c.real_change_in_network(d, "A", "C")
+d.current_first_node = "A"
+d.current_second_node = "C"
+print("Attack From A to C")
 e = c.calculate_business_factor_with_state()
-pprint(e)
+e1 = z1.report_current_state()
+e11 = z1.report_project_state()
+e2 = z2.report_current_state()
+e22 = z2.report_project_state()
+e3 = z3.report_current_state()
+e33 = z3.report_project_state()
+e4 = z4.report_current_state()
+e44 = z4.report_project_state()
+print("real business factor: ", e)
+print("first business factor: ", e1)
+print("first business factor future: ", e11)
+print("second business factor: ", e2)
+print("second business factor future: ", e22)
+print("third business factor: ", e3)
+print("third business factor future: ", e33)
+print("forth business factor: ", e4)
+print("forth business factor future: ", e44)
 
-e = c.real_change_in_network(d, "B", "C")
-print("Attack From B to C")
+e = c.real_change_in_network(d, "C", "E")
+d.current_first_node = "C"
+d.current_second_node = "E"
+print("Attack From C to E")
 e = c.calculate_business_factor_with_state()
-pprint(e)
+e1 = z1.report_current_state()
+e11 = z1.report_project_state()
+e2 = z2.report_current_state()
+e22 = z2.report_project_state()
+e3 = z3.report_current_state()
+e33 = z3.report_project_state()
+e4 = z4.report_current_state()
+e44 = z4.report_project_state()
+print("real business factor: ", e)
+print("first business factor: ", e1)
+print("first business factor future: ", e11)
+print("second business factor: ", e2)
+print("second business factor future: ", e22)
+print("third business factor: ", e3)
+print("third business factor future: ", e33)
+print("forth business factor: ", e4)
+print("forth business factor future: ", e44)
 
-e = c.real_change_in_network(d, "C", "D")
-print("Attack From C to D")
+e = c.real_change_in_network(d, "E", "G")
+d.current_first_node = "E"
+d.current_second_node = "G"
+print("Attack From E to G")
 e = c.calculate_business_factor_with_state()
-pprint(e)
+e1 = z1.report_current_state()
+e11 = z1.report_project_state()
+e2 = z2.report_current_state()
+e22 = z2.report_project_state()
+e3 = z3.report_current_state()
+e33 = z3.report_project_state()
+e4 = z4.report_current_state()
+e44 = z4.report_project_state()
+print("real business factor: ", e)
+print("first business factor: ", e1)
+print("first business factor future: ", e11)
+print("second business factor: ", e2)
+print("second business factor future: ", e22)
+print("third business factor: ", e3)
+print("third business factor future: ", e33)
+print("forth business factor: ", e4)
+print("forth business factor future: ", e44)
+
+e = c.real_change_in_network(d, "G", "I")
+d.current_first_node = "G"
+d.current_second_node = "I"
+print("Attack From G to I")
+e = c.calculate_business_factor_with_state()
+e1 = z1.report_current_state()
+e11 = z1.report_project_state()
+e2 = z2.report_current_state()
+e22 = z2.report_project_state()
+e3 = z3.report_current_state()
+e33 = z3.report_project_state()
+e4 = z4.report_current_state()
+e44 = z4.report_project_state()
+print("real business factor: ", e)
+print("first business factor: ", e1)
+print("first business factor future: ", e11)
+print("second business factor: ", e2)
+print("second business factor future: ", e22)
+print("third business factor: ", e3)
+print("third business factor future: ", e33)
+print("forth business factor: ", e4)
+print("forth business factor future: ", e44)
+
+e = c.real_change_in_network(d, "I", "K")
+d.current_first_node = "I"
+d.current_second_node = "K"
+print("Attack From I to K")
+e = c.calculate_business_factor_with_state()
+e1 = z1.report_current_state()
+e11 = z1.report_project_state()
+e2 = z2.report_current_state()
+e22 = z2.report_project_state()
+e3 = z3.report_current_state()
+e33 = z3.report_project_state()
+e4 = z4.report_current_state()
+e44 = z4.report_project_state()
+print("real business factor: ", e)
+print("first business factor: ", e1)
+print("first business factor future: ", e11)
+print("second business factor: ", e2)
+print("second business factor future: ", e22)
+print("third business factor: ", e3)
+print("third business factor future: ", e33)
+print("forth business factor: ", e4)
+print("forth business factor future: ", e44)
+
+e = c.real_change_in_network(d, "K", "None")
+d.current_first_node = "K"
+d.current_second_node = "None"
+print("Attack From K to None")
+e = c.calculate_business_factor_with_state()
+e1 = z1.report_current_state()
+e11 = z1.report_project_state()
+e2 = z2.report_current_state()
+e22 = z2.report_project_state()
+e3 = z3.report_current_state()
+e33 = z3.report_project_state()
+e4 = z4.report_current_state()
+e44 = z4.report_project_state()
+print("real business factor: ", e)
+print("first business factor: ", e1)
+print("first business factor future: ", e11)
+print("second business factor: ", e2)
+print("second business factor future: ", e22)
+print("third business factor: ", e3)
+print("third business factor future: ", e33)
+print("forth business factor: ", e4)
+print("forth business factor future: ", e44)
 
